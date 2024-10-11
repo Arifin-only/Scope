7f295af6232ecbe5658979e98272fd45f4767bbb
Arifin-only <arifin180104@gmail.com>
2024-10-11 15:38:20 +0800

    Create Scope.dart

---
+++ Scope.dart
@@ -0,0 +1,23 @@
+void main() {
+
+    var name = 'Arifin';
+    
+    void sayHello(){
+    
+    var hello = 'Hello $name';
+    
+    print(hello);
+
+   }
+
+    sayHello();
+    
+    // print(hello); error
+    
+ }
+    
+    void contoh(){
+    
+    // sayHello(); error
+    
+ }
